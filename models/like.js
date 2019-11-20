b8fce76572cd44975962896bcc0251293f5afde4
Hyde Sun <hyde_sun@sina.com>
2019-11-20 16:52:01 +0800

    课程代码8-7

---
+++ models/like.js
@@ -0,0 +1,23 @@
+import {HTTP} from "../utils/http";
+import {config} from "../config";
+
+class LikeModel extends HTTP {
+    like(behavior, art_id, category) {
+        // 判断是点赞还是取消点赞
+        let url = ''
+        if(behavior == 'like') {
+            url = 'like'
+        } else {
+            url = 'like/cancel'
+        }
+        // 发送请求
+        this.request({
+            url: config.api_base_url + url,
+            method: 'POST',
+            data: {
+                art_id: art_id,
+                type: category
+            }
+        })
+    }
+}
