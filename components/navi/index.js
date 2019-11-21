478cc69528cc261efe561f5e8b40e7d4278394c3
Hyde Sun <hyde_sun@sina.com>
2019-11-21 18:09:09 +0800

    课程代码9-1

---
+++ components/navi/index.js
@@ -0,0 +1,37 @@
+// components/navi/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    //标题
+    title: {
+      type: String
+    },
+    // 是否是最新的一条，如果是的话，左箭头图片显示dis那张
+    latest: {
+      type: Boolean
+    },
+    // 是否是第一条，如果是的话，右箭头图片显示dis那张
+    first: {
+      type: Boolean
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    disLeftSrc: "./images/triangle.dis@left.png",
+    disRightSrc: "./images/triangle.dis@right.png",
+    leftSrc: "./images/triangle@left.png",
+    rightSrc: "./images/triangle@right.png"
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
