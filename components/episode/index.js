d51ca1358c3ea10715caa66790f256796e3bafcb
Hyde Sun <hyde_sun@sina.com>
2019-11-21 14:45:58 +0800

    课程代码8-14

--- components/episode/index.js
+++ components/episode/index.js
@@ -9,7 +9,7 @@ Component({
       observer(newVal, oldVal, changedPath) {
         newVal = parseInt(newVal) < 10 ? '0' + newVal : newVal
         this.setData({
-          num: newVal
+          _index: newVal
         })
       }
     }
@@ -21,17 +21,18 @@ Component({
   data: {
     month: null,
     year: null,
-    num: null
+    _index: null,
+    months: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月']
   },
 
   attached: function() {
     // 组件生命周期函数，在组件实例进入页面节点时执行
     let date = new Date()
     // 获取当前月份， 年份
-    let month = date.getMonth() + 1
+    let month = date.getMonth()
     let year = date.getFullYear()
     this.setData({
-      month: month,
+      month: this.data.months[month],
       year: year
     })
   },
