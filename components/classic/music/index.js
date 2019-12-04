7e2dc59ad33805dfff1acc06924135c226d7523d
Hyde Sun <hyde_sun@sina.com>
2019-12-04 15:47:19 +0800

    课程代码10-8

--- components/classic/music/index.js
+++ components/classic/music/index.js
@@ -1,14 +1,20 @@
 // components/classic/music/index.js
 import {classBehavior} from "../classic-beh";
 
+const musicMgr = wx.getBackgroundAudioManager()
+
 Component({
   /**
    * 组件的属性列表
    */
+
   behaviors: [classBehavior],
 
   properties: {
     src: {
+      type: String
+    },
+    title: {
       type: String
     }
   },
@@ -18,13 +24,27 @@ Component({
    */
   data: {
     playSrc: "./images/player@play.png",
-    pauseSrc: "./images/player@pause.png"
+    pauseSrc: "./images/player@pause.png",
+    isPlaying: false
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    onPlay: function (event) {
+      if(this.data.isPlaying) {
+        this.setData({
+          isPlaying: false
+        })
+        musicMgr.pause()
+      } else {
+          this.setData({
+            isPlaying: true
+          })
+        musicMgr.title = this.properties.title
+        musicMgr.src = this.properties.src
+      }
+    }
   }
 })
